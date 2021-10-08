64f4f2c97e309e5095eff827e4bc7f31d08362b1
VarunM <VarunM@betsoftware.com>
2021-10-08 13:59:37 +0200

    removed extra import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import './Quiz'
 import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
       <Quiz/>
-    )
+    );
   }
 }
 
